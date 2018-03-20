39afce98ac0f5f1f3e7f7f02b9b69a59a0e699a6
Andrew Barclay <andrewkbarclay@gmail.com>
2018-03-20 19:43:02 +0000

    added views index, new, show

--- express-server.js
+++ express-server.js
@@ -22,6 +22,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls_new", (req, res) => {
+  res.render("urls_new");
+})
+
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id };
   res.render("urls_show", templateVars);
