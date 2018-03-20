9ca04ada4143a335887af16a2fe2dac55e2614eb
Andrew Barclay <andrewkbarclay@gmail.com>
2018-03-20 18:56:22 +0000

    added header partial

--- express-server.js
+++ express-server.js
@@ -22,6 +22,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
