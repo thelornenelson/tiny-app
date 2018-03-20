6121bee04053a440b95e100dd7b98e07ac2fe37a
Andrew Barclay <andrewkbarclay@gmail.com>
2018-03-20 21:04:11 +0000

    added new url form and redirect functionality

--- express-server.js
+++ express-server.js
@@ -1,8 +1,10 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+let express = require("express");
+let app = express();
+let PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
-var urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -22,15 +24,39 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls_new", (req, res) => {
-  res.render("urls_new");
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new", { random: generateRandomString() });
 })
+
+app.post("/urls", (req, res) => {
+  let newKey = generateRandomString();
+  urlDatabase[newKey] = req.body.longURL;
+  res.statusCode = 303;
+  res.redirect("http://localhost:8080/urls/" + newKey);
+});
 
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id };
   res.render("urls_show", templateVars);
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString(){
+  let length = 6;
+  let result = "";
+  const possible =  "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789";
+  // I've removed some characters that might cause confusion like O/0, I/l/1
+  for (let i = 0; i < length; i++){
+    result += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+
+  return result;
+}
